d65f451cb3c2cf73c8691cdec2f2d5dba44470bd
IngoMiranda <78170022+IngoMiranda@users.noreply.github.com>
2022-08-16 06:57:14 -0300

    menu hamburguer pronto

--- site_faculdade/JS/js.js
+++ site_faculdade/JS/js.js
@@ -2,11 +2,13 @@
 const btnMenu= document.getElementById ("btn-menu");
 
 //função a ser chamada no clique
-function abriMenu(){
+function abriMenu(event){
+    if (event.type === 'touchstart') event.preventDefault ()
     const menu= document.getElementById ("menu01");
     menu.classList.toggle('active')
 }
 
 //ao clicar chamar a função abrimenu
 btnMenu.addEventListener('click', abriMenu);
+btnMenu.addEventListener('touchstart', abriMenu);
 // btnMenu.addEventListener('touch', abriMenu);
